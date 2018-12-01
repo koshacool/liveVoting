afe00c0acabe515cc81a526c0e59d5c20de4a0c0
kroman <roman.kushytskyy@gmail.com>
2018-12-01 21:37:26 +0200

    build

---
+++ api/public/precache-manifest.91e3e46871cc2fbc89bc98732e3c648c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39a03572cc56ff769f2f",
+    "url": "/static/js/main.39a03572.chunk.js"
+  },
+  {
+    "revision": "cc13f4348df39563a1e2",
+    "url": "/static/js/1.cc13f434.chunk.js"
+  },
+  {
+    "revision": "39a03572cc56ff769f2f",
+    "url": "/static/css/main.696b6a7e.chunk.css"
+  },
+  {
+    "revision": "cc13f4348df39563a1e2",
+    "url": "/static/css/1.79f93bb2.chunk.css"
+  },
+  {
+    "revision": "a4af47febc941e72a9ab102701a0255c",
+    "url": "/index.html"
+  }
+];
